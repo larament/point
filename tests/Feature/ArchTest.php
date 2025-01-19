16ff00c759a0e038c204b91be9e5f7c70615be67
iRaziul <raziul.cse@gmail.com>
2025-01-20 00:52:37 +0600

    laravel preset arch testing

--- tests/Feature/ArchTest.php
+++ tests/Feature/ArchTest.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+arch()->preset()->laravel()->ignoring('App\Providers\Filament');
+
 arch('No debugging statements are left in the codebase.')
     ->expect(['dd', 'ddd', 'dump', 'ray'])
     ->not->toBeUsed();
