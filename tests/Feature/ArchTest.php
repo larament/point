6a0a11db6eb4bc28ef4fc1ad3b42e4b38bce6a2c
iRaziul <raziul.cse@gmail.com>
2025-01-20 23:30:48 +0600

    remove duplicate test

--- tests/Feature/ArchTest.php
+++ tests/Feature/ArchTest.php
@@ -3,7 +3,3 @@
 declare(strict_types=1);
 
 arch()->preset()->laravel()->ignoring('App\Providers\Filament');
-
-arch('No debugging statements are left in the codebase.')
-    ->expect(['dd', 'ddd', 'dump', 'ray'])
-    ->not->toBeUsed();
